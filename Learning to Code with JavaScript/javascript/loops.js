d940edd217aaa69e5ce90f5efd9dcbb5091a2c0a
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-02-24 20:23:36 +0500

    loop practice.

---
+++ Learning to Code with JavaScript/javascript/loops.js
@@ -0,0 +1,16 @@
+var x =10;
+for(var i=0;i<x;i++){
+    console.log(i);
+}
+
+/*
+console.log(students[0]);
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+*/
+
+var students = ["jimmy","jack","johnson","jackei"];
+for(var index=0;index<students.length;index+=2){
+    console.log(students[index]);
+}
