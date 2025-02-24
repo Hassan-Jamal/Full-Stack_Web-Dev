7370fa3a9381a4874644f0cdba1bfe12ea83a489
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-02-24 16:33:56 +0500

    Done with comparison Operators.

---
+++ Learning to Code with JavaScript/javascript/comparisonoperator.js
@@ -0,0 +1,33 @@
+var myaccountbalance = 300;
+var coupon = 400;
+var shoesprice = 699;
+// <=, >= operator
+if (myaccountbalance >= shoesprice){
+    myaccountbalance -= shoesprice;
+    console.log("You Just Buy a shoes");
+    console.log("Your account Balance is "+myaccountbalance);
+}
+else if (myaccountbalance+coupon >= shoesprice){
+    //myaccountbalance = myaccountbalance+coupon-shoesprice;
+    myaccountbalance -= shoesprice-coupon;
+    console.log("You Just Buy a shoes with a Coupon."); 
+    console.log("Your account balance is " + myaccountbalance);
+}
+else {
+    console.log("Account Balance is Not enough!!!");
+}
+
+// == operator
+var var1 = 3;
+var var2 = "3";
+if (var1==var2){
+    console.log("These are the same.but may or may not their types.");
+}
+else{
+    console.log("These are not the same..."); 
+}
+
+// === operator
+if(var1===var2){
+    console.log("These are the same even their types.");  //if var2="3" it will be printed in console.
+}
