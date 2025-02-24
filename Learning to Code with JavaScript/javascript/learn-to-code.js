c5b50f2669e7d0a9d28821bb98e16a2ae172f983
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-02-24 15:21:17 +0500

    Done with variables and strings .

---
+++ Learning to Code with JavaScript/javascript/learn-to-code.js
@@ -0,0 +1,17 @@
+
+/* 
+var name = "Hassan Jamal";
+var age = 22;
+var score =  45.6;
+var message = "Hi, My Name is " + name + " and My age is " + age + " and my score is " + score;
+alert(message);
+*/
+var firstname = "Hassan";
+var lastname = "Jamal Bukhari";
+var age = 22;
+var birthdate = "25-08-2003";
+var loginmessage = "Welcome, " + firstname + ". and Happy " + age +"nd Birthday!";
+
+//console 
+console.log(loginmessage);
+       
