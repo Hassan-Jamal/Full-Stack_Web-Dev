50a8d0c21f2f08dc4aa07829999a40e651694571
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-02-25 19:41:42 +0500

    Using Event Listener.

--- 5. Learning to Code with JavaScript/javascript/percentagecalculator.js
+++ 5. Learning to Code with JavaScript/javascript/percentagecalculator.js
@@ -1,4 +1,16 @@
-var numFeild1 = document.getElementById("numFeild1");
-var numFeild2 = document.getElementById("numFeild2");
-var resultFeild = document.getElementById("resultFeild");
+var numFeild1 = document.getElementById('numFeild1');
+var numFeild2 = document.getElementById('numFeild2');
+var resultFeild = document.getElementById('resultFeild');
+var form= document.getElementById('xiswhatpercentofy');
 
+
+form.addEventListener('submit' , function(){
+    if(!numFeild1.value || !numFeild2.value){
+        alert("Please put both Values.");
+    }
+    else{
+        var x = parseFloat(numFeild1.value);
+        var y = parseFloat(numFeild2.value);
+    }
+    
+});
