bf4cc2b2c49638cf36eb2f402dab530f61d181f4
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-02-25 20:29:20 +0500

    Done with Percentage Calculator.

--- 5. Learning to Code with JavaScript/javascript/percentagecalculator.js
+++ 5. Learning to Code with JavaScript/javascript/percentagecalculator.js
@@ -4,13 +4,17 @@ var resultFeild = document.getElementById('resultFeild');
 var form= document.getElementById('xiswhatpercentofy');
 
 
-form.addEventListener('submit' , function(){
+form.addEventListener('submit' , function(event){
     if(!numFeild1.value || !numFeild2.value){
         alert("Please put both Values.");
     }
     else{
         var x = parseFloat(numFeild1.value);
         var y = parseFloat(numFeild2.value);
+        var result=x/y;
+        var percent = result *100;
+        resultFeild.innerText="Answer " + percent +"%";
+        event.preventDefault();
     }
     
 });
