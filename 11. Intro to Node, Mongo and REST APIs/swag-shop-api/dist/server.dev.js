e23582b7aec2c9d00e2ae87b4ad1428f6cb01724
Hassan-Jamal <hassanjamal6577987@gmail.com>
2025-03-01 22:17:11 +0500

    Swag shop API project creation done.

---
+++ 11. Intro to Node, Mongo and REST APIs/swag-shop-api/dist/server.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var express = require("express");
+
+var app = express();
+
+var mongoose = require("mongoose");
+
+var bodyparser = require("body-parser");
+
+var db = mongoose.connect();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+app.listen(3000, function () {
+  console.log("Swag API running on port 3000....");
+});
